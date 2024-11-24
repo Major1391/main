a452d113560df1bc7e65f0a233bd3216ee94a42c
Major1391 <evgeni1391@mail.ru>
2024-11-24 12:43:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1stProgram
+print((9**0.5)*5)
+
+# 2ndProgram
+print(99.9>99.8 and 1000!=1000.1)
+
+# 3rdProgram
+a = (1234%1000//10)
+b = (5678%1000//10)
+print(a+b)
+
+#4thProgram
+a = 13.42
+a1 = (int(a))
+b = 42.13
+b1 = (int(42.13 % 1 * 100))
+a = (int(13.42 * 100 % 100))
+b = (int(42.13))
+print(a1==b1 or a==b)
